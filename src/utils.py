d2fd93aed97762651fc5d7873b88b56f426784af
PrplGreen2 <153992607+PrplGreen2@users.noreply.github.com>
2025-05-15 21:46:38 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -553,7 +553,7 @@ def make_entity_model(sd1, sd2):
                             "1": -1,
                             "2": int(x)
                         }] * plant_sd['Deck']['super'][x]]
-    max_hand = 15
+    max_hand = 150
     max_health = 50
     if '1005433873053' in [plant_id, zombie_id]:
         max_hand = 10
